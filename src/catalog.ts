9f21630805817ed8c06657ac6063b08a46bdffd2
idcmurali1 <idcmurali1@gmail.com>
2025-09-09 15:54:03 -0700

    Changed Hob cap7

--- src/catalog.ts
+++ src/catalog.ts
@@ -19,7 +19,7 @@ export const CATALOG: Product[] = [
     title: 'HOB Dad Cap', 
     price: 22, 
     image: 'https://picsum.photos/seed/cap/640/420',
-    testID: 'product_sku_cap_val1' 
+    testID: 'product_sku_cap_val125' 
   },
   { 
     id: 'sku_bottle', 
